2125b582b564246e3d61d6434820962f5f5973fc
kitkat88 <katherine.peters88@gmail.com>
2025-10-02 15:38:59 -0500

    comments about things to do in ingredients and steps classes

--- RecipeApp/Models/RecipeIngredients.cs
+++ RecipeApp/Models/RecipeIngredients.cs
@@ -1,6 +1,15 @@
 ﻿namespace RecipeApp.Models;
 
 public class RecipeIngredients
-{
+{ 
+    // Data Types:
+        // String ingredient name
+        // integer or fractional amount of ingredient
+        // String ingredientAmountType (e.g. cups, tsp)
     
+    //Set methods
+    
+    
+    //Get methods
+
 }
--- RecipeApp/Models/RecipeSteps.cs
+++ RecipeApp/Models/RecipeSteps.cs
@@ -2,5 +2,10 @@
 
 public class RecipeSteps
 {
-    
+    // Data Type:
+        // Step number
+        // String StepDescription
+        // Picture? if we want pictures to be associated with each step
+        // Integer Length of time per step 
+        // Unit for Time Length
 }
