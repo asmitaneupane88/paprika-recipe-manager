a718d88a999ecb6438c398a1dc9b4b8b67921439
kpeters88 <katherine.peters88@gmail.com>
2025-10-14 12:40:47 -0500

    Be able to input recipe title from recipe page or tab. Tab needs fixing to actually create the new recipe.

--- RecipeApp/Controls/Pages/InputARecipe.xaml.cs
+++ RecipeApp/Controls/Pages/InputARecipe.xaml.cs
@@ -2,6 +2,12 @@
 using Microsoft.UI.Xaml.Controls;
 using RecipeApp.Models;
 using RecipeApp.Services;
+using System.Drawing.Printing;
+using System.Text;
+using Windows.Graphics.Printing;
+using Microsoft.UI.Xaml.Input;
+using RecipeApp.Services;
+using Uno.Extensions;
 
 namespace RecipeApp.Controls.Pages;
 
@@ -10,23 +16,30 @@ public sealed partial class InputARecipe : NavigatorPage
     public InputARecipe(Navigator? nav = null) : base(nav)
     {
         this.InitializeComponent();
+        OnOpening();
     }
     
-    private async void OnButtonAddClick(object sender, RoutedEventArgs e)
+    private async void OnOpening()
     {
-        // Since SavedRecipe inherits from IAutosavingClass, changes are automatically saved
-        // We just need to show a confirmation and navigate back
-        var textbox = new TextBox();
+        //TODO: want this function to work like the click button but need to fix
+        
         var dialog = new ContentDialog
         {
-            Title = "Create Recipe",
-            Content = textbox,
+            Title = "Create New Recipe",
             PrimaryButtonText = "Save",
             CloseButtonText = "Cancel",
             DefaultButton = ContentDialogButton.Primary,
             XamlRoot = this.XamlRoot
         };
-
+        var textbox = new TextBox()
+        {
+            PlaceholderText="Recipe Title",
+        };
+        textbox.TextChanged += (s, _) =>
+        {
+            dialog.IsPrimaryButtonEnabled = !string.IsNullOrWhiteSpace(textbox.Text);
+        };
+        dialog.Content = textbox;
         var result = await dialog.ShowAsync();
         if (result == ContentDialogResult.Primary)
         {
