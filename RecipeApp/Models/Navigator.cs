e56daddc9072d1d6807691dd11578e255ecad01d
William Herbert <Vastspacewill@gmail.com>
2025-10-27 16:21:23 -0500

    Revert "Pantry Ingredients"
    This reverts commit 22c8b01e5c03acd5e3cfafaceb8746bce8f3c644.

--- RecipeApp/Models/Navigator.cs
+++ RecipeApp/Models/Navigator.cs
@@ -49,12 +49,6 @@ public partial class Navigator : ObservableObject
             Name = "Web Browser",
             PageFactory = nav => new WebBrowserPage(nav),
         },
-        new()
-        {
-            Icon = Symbol.Library,
-            Name = "My Pantry",
-            PageFactory = nav => new PantryIngredientsPage(nav),
-        },
     ];
     
     /// <summary>
