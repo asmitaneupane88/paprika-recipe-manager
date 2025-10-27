388bc30a092316e64ea6e119843f42941b9a7954
William Herbert <Vastspacewill@gmail.com>
2025-10-27 16:54:57 -0500

    got the pantry page finished I think

--- RecipeApp/Controls/Pages/PantryIngredientsPage.xaml.cs
+++ RecipeApp/Controls/Pages/PantryIngredientsPage.xaml.cs
@@ -1,39 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System.Drawing.Printing;
-using System.Text;
-using Windows.Graphics.Printing;
-using Microsoft.UI.Xaml.Input;
-using RecipeApp.Services;
-using Uno.Extensions;
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+﻿
 
 namespace RecipeApp.Controls.Pages;
 
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
+
 public sealed partial class PantryIngredientsPage : NavigatorPage
 {
     [ObservableProperty] private partial ObservableCollection<IngredientCard> AllIngredients { get; set; } = [];
     public PantryIngredientsPage(Navigator? nav) : base(nav)
     {
         this.InitializeComponent();
+
+        _ = ShowIngredients();
     }
 
-    private async Task ShowIngredients ()
+    private async Task ShowIngredients()
     {
         AllIngredients = (await RecipeIngredient.GetAll())
             .Select(i => new IngredientCard() { Ingredient = i, IsSelected = false})
@@ -53,10 +33,26 @@ public sealed partial class PantryIngredientsPage : NavigatorPage
             .Any(c => c.IsSelected);
     }
     
-    private RecipeCard[] GetSelectedIngredientCards()
+    private List<IngredientCard> GetSelectedIngredientCards()
     {
         return AllIngredients
             .Where(c => c.IsSelected)
-            .ToArray();
+            .ToList();
+    }
+
+    private async void ButtonAddIngredient_OnClick(object sender, RoutedEventArgs e)
+    {
+        var newIngredient = new RecipeIngredient();
+        
+        AllIngredients.Add(new IngredientCard() { Ingredient = newIngredient, IsSelected = false });
+        await RecipeIngredient.Add(newIngredient);
+    }
+
+    private async void ButtonRemoveIngredient_OnClick(object sender, RoutedEventArgs e)
+    {
+        var selected = GetSelectedIngredientCards();
+        
+        await Task.WhenAll(selected.Select(c => RecipeIngredient.Remove(c.Ingredient)));
+        selected.ForEach(i => AllIngredients.Remove(i));
     }
 }
