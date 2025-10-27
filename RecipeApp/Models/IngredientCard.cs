22c8b01e5c03acd5e3cfafaceb8746bce8f3c644
kpeters88 <katherine.peters88@gmail.com>
2025-10-27 13:58:07 -0500

    Pantry Ingredients

---
+++ RecipeApp/Models/IngredientCard.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeApp.Models;
+
+/// <summary>
+/// For use by the <see cref="RecipeListPage"/> to display recipes and allow multi-selecting recipes.
+/// </summary>
+public partial class IngredientCard : ObservableObject
+{
+    [ObservableProperty] public required partial RecipeIngredient Ingredient { get; set; }
+    [ObservableProperty] public required partial bool IsSelected { get; set; } = false;
+}
