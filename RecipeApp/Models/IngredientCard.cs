b959f4c33bbf8ffaf33c6d4dd30882a1216f3a26
kpeters88 <katherine.peters88@gmail.com>
2025-11-02 22:01:33 -0600

    Fixed Grocery and Pantry Lists so they are separate lists instead of copies of one another

--- RecipeApp/Models/IngredientCard.cs
+++ RecipeApp/Models/IngredientCard.cs
@@ -2,6 +2,7 @@
 
 public partial class IngredientCard : ObservableObject
 {
+    [ObservableProperty] public required partial PantryIngredient PIngredient { get; set; }
     [ObservableProperty] public required partial RecipeIngredient Ingredient { get; set; }
     [ObservableProperty] public required partial bool IsSelected { get; set; } = false;
 }
