03f73011cd7f48b7ced1004a16867b0a868a36d0
William Herbert <Vastspacewill@gmail.com>
2025-11-10 20:36:33 -0600

    merge with master caused a lot of issues, I need to swap revisions of this branch to try and find a "new" file that got removed from the merge

--- RecipeApp/Controls/Pages/RecipeListPage.xaml.cs
+++ RecipeApp/Controls/Pages/RecipeListPage.xaml.cs
@@ -135,7 +135,7 @@ public sealed partial class RecipeListPage : NavigatorPage
         if (sender is ListView { SelectedItem: RecipeCard rc } lv)
         {
             lv.SelectedItem = null;
-            var details = new RecipeDetailsPage(Navigator, savedRecipe: rc.SavedRecipe);
+            var details = new RecipeDetailsV2(Navigator, rc.SavedRecipe);
             Navigator.Navigate(details, $"Recipe: {rc.SavedRecipe.Title}");
         }
     }
