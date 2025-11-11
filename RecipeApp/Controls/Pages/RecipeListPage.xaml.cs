0336fa0633671a6dac960fc8b54402afee8f3184
William Herbert <Vastspacewill@gmail.com>
2025-11-11 13:18:54 -0600

    button to go to the recipe creation chat page

--- RecipeApp/Controls/Pages/RecipeListPage.xaml.cs
+++ RecipeApp/Controls/Pages/RecipeListPage.xaml.cs
@@ -1,24 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
-
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Input;
-
-using Windows.Storage;
-using Windows.Storage.Pickers;
-
-using RecipeApp.Models;
+﻿using Microsoft.UI.Xaml.Input;
 using RecipeApp.Services;
-using Uno.Extensions;
-
-using System.Drawing.Printing;
-using Windows.Devices.Midi;
-using Windows.Graphics.Printing;
 
 namespace RecipeApp.Controls.Pages;
 
@@ -325,9 +306,10 @@ public sealed partial class RecipeListPage : NavigatorPage
 
     }
     
-    private void OnButtonGroceryListClick(object sender, RoutedEventArgs e)
+    private void OnButtonChatClick(object sender, RoutedEventArgs e)
     {
-        //TODO: create list from selected and open it
+        var page = new RecipeChat(Navigator);
+        Navigator.Navigate(page, "Create Ai Recipe");
     }
 
     private async void OnButtonPrintClick(object sender, RoutedEventArgs e)
